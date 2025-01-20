916333c99e5bef6cf19e3279050dec36aab0b8fb
André Araújo <andremendoncamineiro@gmail.com>
2025-01-20 01:56:11 -0300

    .

--- backend/models/colaborador.py
+++ backend/models/colaborador.py
@@ -12,14 +12,3 @@ class Colaborador(Pessoa):
         except Exception as e:
             print(f"Erro ao editar PPC: {e}")
 
-class Avaliador(Pessoa):
-    def avaliarPPC(self, conexao, ppc_id, aprovado, motivo_rejeicao=None):
-        """
-        Avalia o PPC, aprovando ou rejeitando.
-        """
-        try:
-            status = "Aprovado" if aprovado else "Rejeitado"
-            PPCCrud.atualizar(conexao, ppc_id, status=status, motivo_rejeicao=motivo_rejeicao)
-            print(f"PPC ID {ppc_id} avaliado como '{status}' pelo avaliador {self.nome}.")
-        except Exception as e:
-            print(f"Erro ao avaliar PPC: {e}")
